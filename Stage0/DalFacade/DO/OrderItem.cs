d03ee24868a7d91d6c682cf207bbe7fc8b713481
mendiwelner <meniwell@gmail.com>
2022-11-01 21:43:50 +0200

    מימוש מחלקות

--- Stage0/DalFacade/DO/OrderItem.cs
+++ Stage0/DalFacade/DO/OrderItem.cs
@@ -1,6 +1,19 @@
 ﻿
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct OrderItem
 {
+    public int ProductID { get; set; }
+    public int OrderID { get; set; }
+    public double Price { get; set; }
+    public int Amount { get; set; }
+
+    public override string ToString() => $@"
+        Product ID={ID}: {Name}, 
+        category - {Category}
+    	Price: {Price}
+    	Amount in stock: {InStock}";
+
 }
--- Stage0/DalFacade/DO/Product.cs
+++ Stage0/DalFacade/DO/Product.cs
@@ -1,6 +1,13 @@
 ﻿
+using System.ComponentModel;
+
 namespace DO;
 
 public struct Product
 {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public CategoryAttribute Category { get; set; }
+    public int InStock { get; set; }
 }
