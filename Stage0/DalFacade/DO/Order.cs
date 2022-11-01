838ee247e1ec68f0cfdccc33fe253ad0b55820fb
Moshe Roimicher <roimichercode@gmail.com>
2022-11-01 21:45:35 +0200

    moshe up

--- Stage0/DalFacade/DO/Order.cs
+++ Stage0/DalFacade/DO/Order.cs
@@ -1,6 +1,25 @@
 ﻿
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct Order
 {
+    public int ID { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomeAdress { get; set; }
+    DateTime OrderDate { get; set; }
+    DateTime ShipDate { get; set; }
+    DateTime DeliveryDate { get; set; }
+
+    public override string ToString() => $@"
+        Product ID={ID}: {Name}, 
+        category - {Category}
+    	Price: {Price}
+    	Amount in stock: {InStock}
+    ";
 }
+
--- Stage0/DalList/DalOrder.cs
+++ Stage0/DalList/DalOrder.cs
@@ -1,6 +1,9 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
 namespace Dal;
 
 public class DalOrder
 {
+
 }
