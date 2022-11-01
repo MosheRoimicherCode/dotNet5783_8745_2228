1be8574960c1d1d036c12b9a38c90ac2ef3c737e
Moshe Roimicher <roimichercode@gmail.com>
2022-11-02 00:32:24 +0200

    data source

--- Stage0/DalList/DataSource.cs
+++ Stage0/DalList/DataSource.cs
@@ -6,8 +6,10 @@ namespace Dal;
 
 internal static class DataSource
 {
-    static internal readonly RandomNumberGenerator RandomNum = RandomNumberGenerator.Create();
-    static internal Array ProductArray = new Array(Array.Empty<Product>(50));
-    static internal Array ProductArray = new Array()
+    static internal readonly RandomNumberGenerator _randomNum = RandomNumberGenerator.Create();
+    static internal Product[] _productArr = new Product[50];
+    static internal Order[] _orderArr = new Order[100];
+    static internal OrderItem[] _orderItemArr = new OrderItem[200];
+
 
 }
