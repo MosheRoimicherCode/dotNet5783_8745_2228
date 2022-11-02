560e51b7a2029742c27aead1b64207646e7733d8
Moshe Roimicher <roimichercode@gmail.com>
2022-11-02 17:12:10 +0200

    add function of product array

--- Stage0/DalList/DataSource.cs
+++ Stage0/DalList/DataSource.cs
@@ -11,5 +11,9 @@ internal static class DataSource
     static internal Order[] _orderArr = new Order[100];
     static internal OrderItem[] _orderItemArr = new OrderItem[200];
 
-
+    static internal int _productArrIndex = 0;
+    static void AddProduct(Product array, int index)
+    {
+        _productArr[index] = array;
+    }
 }
