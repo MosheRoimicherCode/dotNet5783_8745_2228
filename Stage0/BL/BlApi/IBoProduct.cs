b3aead080a5f2308530c8581842fe89f2ee4b7c6
Moshe Roimicher <roimichercode@gmail.com>
2022-11-21 02:32:33 +0200

    startWithLogicalIBoProduct

---
+++ Stage0/BL/BlApi/IBoProduct.cs
@@ -0,0 +1,27 @@
+﻿using BO;
+namespace BlApi
+{
+    public interface IBoProduct
+    {
+        ///add product to data base - for manager user
+        public void Add(BoProduct item);
+
+        ///search for a product with specific Id for manager user 
+        /// <returns> IBoProduct item </returns>
+        public BoProduct Get(int Id);
+
+        ///search for a product with specific Id inside client cart (for client user)
+        /// <returns> BoProductItem </returns>
+        public BoProductItem Get(int Id, BoCart cart);
+
+        ///remove product from data base - for manager yser
+        public void Remove(int Id);
+
+        ///update data for product in data base
+        public void Update(BoProduct item);
+
+        /// return a lost with all product in client order
+        /// <returns> Product list </returns>
+        public BoProductForList GetLists();
+    }
+} /// interface of product items for manager and client
---
+++ Stage0/BL/BlImplementation/BoProduct.cs
@@ -0,0 +1,43 @@
+﻿using BlApi;
+using Dal;
+using DalApi;
+using DO;
+
+namespace BlImplementation
+{
+    internal class BoProduct : IBoProduct
+    {
+        IDal Dal = new DalList ();
+
+        public void Add(BO.BoProduct item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BO.BoProduct Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BO.BoProductItem Get(int Id, BO.BoCart cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(BO.BoProduct item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BO.BoProductForList GetLists()
+        {
+            BO.BoProductForList product = new BO.BoProductForList;
+            return product;
+        }
+    }
+}
