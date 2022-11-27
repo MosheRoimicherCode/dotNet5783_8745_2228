387a966c8e117e30cf8928d87f8d21bfd342510d
Moshe Roimicher <roimichercode@gmail.com>
2022-11-27 19:15:01 +0200

    exeptionDAlaPi

--- Stage0/DalFacade/DalApi/Exeptions.cs
+++ Stage0/DalFacade/DalApi/Exeptions.cs
@@ -1,16 +1,30 @@
-﻿namespace DalApi
+﻿using System.Runtime.Serialization;
+
+namespace DalApi
 
 {
-    public class IdException : Exception
+
+    [Serializable]
+    internal class IdException : Exception
     {
-        static public string IdError = "not found id";
-        public IdException(string str) { IdError = str; }
+        public IdException() : base() { }
+        public IdException(string message) : base(message) { }
+        public IdException(string message, Exception innerException) : base(message, innerException) { }
+        protected IdException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public override string ToString() => base.ToString();
     }
 
-    public class ProgramExit : Exception
+
+    [Serializable]
+    internal class ProgramExit : Exception
     {
-        static public string Exit = "Exit Program";
-        public ProgramExit() {}
+        public ProgramExit() : base() { }
+        public ProgramExit(string message) : base(message) { }
+        public ProgramExit(string message, Exception innerException) : base(message, innerException) { }
+        protected ProgramExit(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public override string ToString() => base.ToString();
     }
-    
+
 }
