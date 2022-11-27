bcdda37f9a87dc7c81c1e567cbd53376d31c8a0a
Moshe Roimicher <roimichercode@gmail.com>
2022-11-27 03:41:02 +0200

    .

--- Stage0/BL/BO/BoOrderTracking .cs
+++ Stage0/BL/BO/BoOrderTracking .cs
@@ -2,6 +2,8 @@
 using DO;
 using System.Collections.Generic;
 using static BO.Enums;
+using static System.Formats.Asn1.AsnWriter;
+
 public class BoOrderTracking
 {
 
@@ -10,12 +12,26 @@ public class BoOrderTracking
     public Status Status { get; set; }
     public List<Tuple<DateTime, string>> TupleList = new List<Tuple<DateTime, string>>();
 
-    /// funcs
-    public override string ToString() => $@"
-        OrderID: {OrderID}
-        Category: {Status}
-        list: {TupleList.ToString()}
-    ";
+    ///// funcs
+    //public override string ToString() => $@"
+    //    OrderID: {OrderID}
+    //    Category: {Status}
+    //    list: {}
+
+    //";
+
+    public override string ToString()
+    {
+        
+        Console.WriteLine("OrderID: ");
+        Console.WriteLine(OrderID);
+        Console.WriteLine("Category: ");
+        Console.WriteLine(Status);
+        Console.WriteLine("list: ");
+        foreach (var score in TupleList)
+        { Console.WriteLine(score.ToString()); }
+    }
+
 
 }
 
