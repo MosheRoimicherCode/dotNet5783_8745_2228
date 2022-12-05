bb3de300f631be7b02c247c0340d007665d3c128
Moshe Roimicher <roimichercode@gmail.com>
2022-12-05 18:35:13 +0200

    DalProductFix

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -7,11 +7,8 @@ using static Dal.DataSource;
 ///A class for connect with Product struck
 internal class DalProduct : IProduct
 {
-    ///----------------------------------------------------
-    ///----------------- CRUD functions -------------------
-
     public int Add(Product product) =>
-        DataSource._productList.Exists(p => p?.ID == product.ID)
+        DataSource._productList.Exists(productInList => productInList?.ID == product.ID)
             ? throw new IdException("Product ID already exists")
             : DataSource.AddProduct(product); /// Add Product to Data Base
 
@@ -19,35 +16,28 @@ internal class DalProduct : IProduct
     {
         foreach (var product in from Product product in _productList
                                 where product.ID == ProductID
-                                select product)
-        {
+                                select product) return product;
 
-            return product;
-        }
-
-        ///in case of Id not found, throw exception
-        throw new IdException(" Not found ID. (DalOrderProduct.Get Exception)");
+        throw new IdException(" Not found ID. (DalProduct.Get Exception)");
     }///search for product by Id and return the specific product
 
-    public Product? Get(Func<Product?, bool> f) =>
-        (from product in _productList
-         where f(product)
-         select product).FirstOrDefault();
+    public Product? Get(Func<Product?, bool> filter) =>
+                                                    (from product in _productList
+                                                    where filter(product)
+                                                    select product).FirstOrDefault();
 
     public void Delete(int ProductID)
     {
         bool flag = false;
         for (int i = 0; i < _productList.Count; i++)
         {
-            if (_productList[i].ID == ProductID)
+            if (_productList[i]?.ID == ProductID)
             {
                 _productList.Remove(_productList[i]);
                 flag = true;
             }
-
-            ///if not found return a message
-
         }
+        //if Id not found send a MESSAGE
         if (flag == false) Console.WriteLine(" Not found ID. (DalProduct.Delete Exception)");
         ///delete product from data base by Id
     }
@@ -56,32 +46,20 @@ internal class DalProduct : IProduct
     {
         for (int i = 0; i < _productList.Count; i++)
         {
-            var product = _productList[i];
-            if (product.ID.Equals(ProductID))
+            Product? product = new();
+            product = _productList[i];
+            if (product?.ID.Equals(ProductID) ?? throw new IdException(" Not found ID. (DalOrderProduct.Update Exception)"))
             {
                 int index = _productList.IndexOf(product);
                 _productList.RemoveAt(index);
                 _productList.Insert(index, newProduct);
                 return;
             }
-
         }
-        ///if not found return a message
-        throw new IdException(" Not found ID. (DalOrderProduct.Update Exception)");
     }///replace order by another inside array
 
+    public IEnumerable<Product?> GetAll(Func<Product?, bool>? filter) =>
+        filter == null ? _productList.Select(prouductInList => prouductInList)
+                  : _productList.Where(filter);
 
-    ///----------------------------------------------------
-    ///----------------------------------------------------
-
-
-
-    ///----------------------------------------------------
-    ///----------------- Methods -------------------------- 
-
-    public IEnumerable<Product?> GetAll(Func<Product?, bool>? f) =>
-        f == null ? _productList.Select(p => p)
-                  : _productList.Where(f);
-
-    ///----------------------------------------------------
 }
