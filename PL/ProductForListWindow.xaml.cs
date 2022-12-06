6dd970ac23140134be4b338909d9c40cad8003a2
Moshe Roimicher <roimichercode@gmail.com>
2022-12-06 16:33:55 +0200

    .

--- PL/ProductForListWindow.xaml.cs
+++ PL/ProductForListWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using BlImplementation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace PL
 {
@@ -28,7 +18,7 @@ namespace PL
             InitializeComponent();
             
             
-            ProductListview.ItemsSource = p.BoProduct.GetList();
+            ProductListview.ItemsSource = p.Product.GetList();
             CategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Enums.Category));
 
             //ProductListview.ItemsSource = p.BoProduct.GetList(CategorySelector.ItemsSource, true);
