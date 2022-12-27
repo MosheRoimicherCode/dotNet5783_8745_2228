f504ea951677e8e7c1a80576038e22ac40ae41f1
Moshe Roimicher <roimichercode@gmail.com>
2022-12-27 20:33:45 +0200

    linq finish

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -13,24 +13,12 @@ internal class DalProduct : IProduct
             : AddProduct(product); /// Add Product to Data Base
     public Product? Get(Func<Product?, bool> filter) => (from product in _productList
                                                          where filter(product)
-                                                         select product).FirstOrDefault();
+                                                         orderby (product.Value.ID)
+                                                         select product.Value).FirstOrDefault();
     public void Delete(int ProductID)
     {
         try { _productList.RemoveAll(x => x?.ID == ProductID); }
         catch (ArgumentNullException) { throw new IdException(" Not found ID. (Dalproduct.Delete Exception)"); }
-
-        //bool flag = false;
-        //for (int i = 0; i < _productList.Count; i++)
-        //{
-        //    if (_productList[i]?.ID == ProductID)
-        //    {
-        //        _productList.Remove(_productList[i]);
-        //        flag = true;
-        //    }
-        //}
-        ////if Id not found send a MESSAGE
-        //if (flag == false) Console.WriteLine(" Not found ID. (DalProduct.Delete)");
-        ///delete product from data base by Id
     }
     public void Update(int ProductID, Product newProduct)
     {
