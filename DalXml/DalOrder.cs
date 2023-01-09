c10b336e0bcfd8852bcb8fe9421408381f0cfb48
Moshe Roimicher <roimichercode@gmail.com>
2023-01-09 17:40:30 +0200

    startstag6

---
+++ DalXml/DalOrder.cs
@@ -0,0 +1,34 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class DalOrder : IOrder
+{
+    public int Add(Order t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int n)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? Get(Func<Order?, bool> f)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order?> GetAll(Func<Order?, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(int n, Order t)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ DalXml/DalXml.cs
@@ -0,0 +1,18 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal class DalXml : IDal
+    {
+        public IProduct Product => throw new NotImplementedException();
+
+        public IOrder Order => throw new NotImplementedException();
+
+        public IOrderItem OrderItem => throw new NotImplementedException();
+    }
+}
