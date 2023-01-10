9754b298feb65866bbb06817a4948f290a473921
Moshe Roimicher <roimichercode@gmail.com>
2023-01-10 13:12:39 +0200

    XML.dalOrder.function

--- DalXml/DalOrder.cs
+++ DalXml/DalOrder.cs
@@ -6,31 +6,53 @@ using System.Xml.Linq;
 internal class DalOrder : IOrder
 {
     string path = @"..\xml\orders.xml";
-    public int Add(Order t)
+    XElement dataBaseOrders = XElement.Load(@"..\xml\orders.xml"); //copy data base to code
+    public int Add(Order order)
     {
-        XElement newOrder = XElement.Load(path);
-        return 0;    
-           
+        dataBaseOrders.Add(order); //add new item
+        dataBaseOrders.Save(path); //save changes
+        return order.ID; //return idOrder
     }
 
-    public void Delete(int n)
+    public void Delete(int ID)
     {
-        throw new NotImplementedException();
+        try
+        {
+            XElement? newDataBase = (from order2 in dataBaseOrders.Elements()
+                                     where Convert.ToInt32(order2.Element("ID")?.Value) == ID
+                                     select order2).FirstOrDefault(); //search element with received id
+            newDataBase?.Remove();   //remove from copy
+            newDataBase?.Save(path); //save changes to original
+        }
+        catch { }
     }
 
-    public Order? Get(Func<Order?, bool> f)
+    public Order? Get(Func<Order?, bool> filter) => (from order in createListFromXml()
+                                                     where filter(order)
+                                                     select order).FirstOrDefault();
+
+    public IEnumerable<Order?> GetAll(Func<Order?, bool> filter) => from order in createListFromXml()
+                                                                    where filter(order)
+                                                                    select order;
+
+    public void Update(Order order)
     {
-        throw new NotImplementedException();
+        Delete(order.ID);
+        Add(order);
     }
 
-    public IEnumerable<Order?> GetAll(Func<Order?, bool>? filter = null)
-    {
-        throw new NotImplementedException();
-    }
 
-    public void Update(Order t)
-    {
-        throw new NotImplementedException();
-    }
+//private methods
+    IEnumerable<Order?> createListFromXml() => (IEnumerable<Order?>)(from order in dataBaseOrders.Elements()
+                                              select new DO.Order()
+                                              {
+                                                  ID = Convert.ToInt32(order.Element("ID")?.Value),
+                                                  CustomerName = order.Element("Name")?.Value,
+                                                  CustomeAdress = order.Element("CustomeAdress")?.Value,
+                                                  CustomerEmail = order.Element("CustomerEmail")?.Value,
+                                                  DeliveryDate = Convert.ToDateTime(order.Element("OrderDate")?.Value),
+                                                  ShipDate = Convert.ToDateTime(order.Element("ShipDate")?.Value),
+                                                  OrderDate = Convert.ToDateTime(order.Element("OrderDate")?.Value)
+                                              });
 }
 
