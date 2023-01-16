9773b87fa3d743c8d828b55906b774b6937e610c
Moshe Roimicher <roimichercode@gmail.com>
2023-01-16 19:19:11 +0200

    .

---
+++ PL/Simulator/SimulatorWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace PL;
+
+using BlApi;
+using System.Windows;
+
+public partial class SimulatorWindow : Window
+{
+    static readonly IBl bl = BlApi.Factory.Get();
+    public SimulatorWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void StartSimulation(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
