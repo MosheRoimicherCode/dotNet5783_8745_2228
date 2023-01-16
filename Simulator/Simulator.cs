44ea4329fd253f2ebd34d61b6a38685974b2d8b0
Moshe Roimicher <roimichercode@gmail.com>
2023-01-16 12:48:36 +0200

    .

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -2,23 +2,22 @@
 
 static public class Simulator
 {
-    private static volatile bool flag;
+    private static volatile bool flagForStopSimulator;
     private static void RunSimulator()
     {
-        flag = true;
-        while (flag)
+        flagForStopSimulator = true;
+        while (flagForStopSimulator)
         {
             Console.WriteLine(Thread.CurrentThread.Name);
             Thread.Sleep(1000);
-            if (somethingHapended()) flag = false;
-            
         }
     }
     static bool somethingHapended() => true;
-
-
     static Thread simulator = new(RunSimulator); //create Thread that make something
+    
     public static void ActivatorSimulator() => simulator.Start(); //public method to start simulator
+    public static void StopSimulator(bool flag) => flagForStopSimulator = flag; //public method to stop simulator
+
 
     static private event EventHandler StopThread;
     static private event EventHandler NextEvent;
